dd06ffa486aa633a79fd6e31bddb9d05264c4e7c
Frank Denis <github@pureftpd.org>
2026-08-31 13:30:02 +0200

    Bump

--- Formula/swival.rb
+++ Formula/swival.rb
@@ -1,8 +1,8 @@
 class Swival < Formula
   desc "Small, powerful CLI coding agent for open AI models"
   homepage "https://swival.dev/"
-  url "https://github.com/Swival/swival/archive/refs/tags/1.0.41.tar.gz"
-  sha256 "73052a3dd27624270210f9c54abb6c34b97a11eeb57f209c40d522608af8a146"
+  url "https://github.com/Swival/swival/archive/refs/tags/1.0.42.tar.gz"
+  sha256 "911ee28bf073bbc0c40c366a248ddc58fba511ea26f32f096f1226cf6e201dd8"
   license "MIT"
 
   depends_on "go" => :build
